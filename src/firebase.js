599a1fa7d63e496c743c69f4ddc5647d1e44688d
YoAngelCruz <angelcruz.feb@gmail.com>
2023-05-31 17:07:06 -0600

    create branch login

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat/app"
 import "firebase/compat/firestore"
 import "firebase/compat/storage"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDtP-rpoXfO3yvGVH79e1XiI-TGGkC_JZ8",
@@ -14,7 +15,9 @@ const firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firestore=firebaseApp.firestore();
 const storage = firebase.storage();
+const auth = getAuth(firebaseApp);
+const provider = new GoogleAuthProvider();
 
 export { 
-  storage, firestore as default
+  storage, firestore as default, auth, provider
 }
